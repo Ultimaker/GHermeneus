0d1853145373722e8624c2cc524f13029693b8cd
Jelle Spijker <j.spijker@ultimaker.com>
2020-05-29 17:00:50 +0200

    Implemented Conan package manager
    Conan is a dependency and package manager for C and
     C++ languages. Added the dependecies:
     - gtest (build)
     - Eigen3
     - ranges-v3
    https://docs.conan.io/en/latest/introduction.html

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+import platform
+
+
+class GHermeneusConan(ConanFile):
+    name = "GHermeneus"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "Jelle Spijker <j.spijker@ultimaker.com>"
+    url = "https://github.com/Ultimaker/GHermeneus"
+    description = "Fast GCode interpreter"
+    topics = ("GCode", "3D printing", "Path generator", "Ultimaker", "Cura")
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "eigen/3.3.7@conan/stable", "range-v3/0.10.0@ericniebler/stable"
+    build_requires = "gtest/1.10.0"
+    options = {"shared": [True, False], "build_tests": [True, False]}
+    default_options = {"shared": False, "build_tests": True}
+    generators = "cmake"
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTS"] = self.options.build_tests
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        if self.options.build_tests:
+            cmake.test()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="GHermeneus")
+        self.copy("*GHermeneus.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.name = "GCodeHermeneus"
+        self.cpp_info.libs = tools.collect_libs(self)
+
