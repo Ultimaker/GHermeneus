8bbb8c90d7e4c7445faf4ad888c90fe193c9042b
Jelle Spijker <j.spijker@ultimaker.com>
2020-09-04 17:12:22 +0200

    Added benchmark to test parallel vs sequence
    Run on (8 X 4600 MHz CPU s)
    CPU Caches:
      L1 Data 32K (x4)
      L1 Instruction 32K (x4)
      L2 Unified 256K (x4)
      L3 Unified 8192K (x1)
    Load Average: 2.06, 1.89, 2.16
    ----------------------------------------------------------------------------------
    Benchmark                        Time             CPU   Iterations UserCounters...
    ----------------------------------------------------------------------------------
    BM_simple_file_parallel   93955204 ns     93698823 ns            7 items_per_second=407.63k/s
    BM_simple_file           155943698 ns    155577078 ns            4 items_per_second=429.628k/s
    BM_big_file_parallel     512463419 ns    510904727 ns            1 items_per_second=3.21943M/s
    BM_big_file              908316609 ns    906724479 ns            1 items_per_second=1.81402M/s

--- benchmark/bm_machine.cpp
+++ benchmark/bm_machine.cpp
@@ -4,12 +4,88 @@
 
 #include <benchmark/benchmark.h>
 
-static void BM_placeholder_test(benchmark::State& state) {
-    // init test
-    for (auto _ : state) {
-        // time code
-    }
+#include <string_view>
+#include <iostream>
+#include <fstream>
+#include <functional>
+
+#include "GHermeneus/GHermeneus.h"
+
+const static std::string simple_file{"../resources/simple.gcode"};
+const static std::string big_file{"../resources/big.gcode"};
+
+size_t get_nol(const std::string& filename)
+{
+    std::ifstream gcode_file(filename);
+    std::string line;
+    size_t nol{0};
+    while (std::getline(gcode_file, line))
+        nol++;
+    return nol;
 }
-BENCHMARK(BM_placeholder_test);
+
+using namespace GHermeneus::Dialects::Marlin;
+
+static void BM_simple_file_parallel(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto UM3 = MarlinMachine();
+        state.ResumeTiming();
+        std::ifstream gcode_file(simple_file);
+        UM3 << gcode_file;
+    }
+    state.SetItemsProcessed(get_nol(simple_file));
+}
+
+BENCHMARK(BM_simple_file_parallel);
+
+static void BM_simple_file(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto UM3 = MarlinMachine();
+        UM3.setParallelExecution(false);
+        state.ResumeTiming();
+        std::ifstream gcode_file(simple_file);
+        UM3 << gcode_file;
+    }
+    state.SetItemsProcessed(get_nol(simple_file));
+}
+
+BENCHMARK(BM_simple_file);
+
+static void BM_big_file_parallel(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto UM3 = MarlinMachine();
+        state.ResumeTiming();
+        std::ifstream gcode_file(big_file);
+        UM3 << gcode_file;
+    }
+    state.SetItemsProcessed(get_nol(big_file));
+}
+
+BENCHMARK(BM_big_file_parallel);
+
+static void BM_big_file(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        auto UM3 = MarlinMachine();
+        UM3.setParallelExecution(false);
+        state.ResumeTiming();
+        std::ifstream gcode_file(big_file);
+        UM3 << gcode_file;
+    }
+    state.SetItemsProcessed(get_nol(big_file));
+}
+
+BENCHMARK(BM_big_file);
 
 BENCHMARK_MAIN();
