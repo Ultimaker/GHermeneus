dd35251d80c6c17fe7832d49a69e73cd695961e8
Jelle Spijker <j.spijker@ultimaker.com>
2020-10-18 20:26:00 +0200

    Added constexpr test for Primitive types
    CURA-7699_primitives_should_know_their_history

--- tests/constexpr_tests.cpp
+++ tests/constexpr_tests.cpp
@@ -4,15 +4,70 @@
 
 #include <catch2/catch.hpp>
 
-constexpr unsigned int Factorial(unsigned int number)
+#include "GHermeneus/Primitive.h"
+using primitive_double = GHermeneus::Primitive<double>;
+
+#include <spdlog/spdlog.h>
+constexpr spdlog::level::level_enum log_level = spdlog::level::critical;
+
+TEST_CASE("Relational constexpr", "[primitive]")
 {
-    return number <= 1 ? number : Factorial(number - 1) * number;
+    spdlog::set_level(log_level);
+    using primitive = GHermeneus::Primitive<double>;
+
+    constexpr primitive p1{ 1 };
+    constexpr primitive p2{ 2 };
+    constexpr primitive p3{ 1 };
+
+    SECTION("Equal")
+    {
+        STATIC_REQUIRE(p1 != p2);
+        STATIC_REQUIRE(p1 == p3);
+        STATIC_REQUIRE(p1 == p1);
+    }
+
+    SECTION("Equal different type")
+    {
+        STATIC_REQUIRE(p1 != 1.1);
+        STATIC_REQUIRE(1.1 != p1);
+        STATIC_REQUIRE(p1 == 1.);
+        STATIC_REQUIRE(1. == p1);
+    }
+
+    SECTION("Greater then")
+    {
+        STATIC_REQUIRE(p1 < p2);
+        STATIC_REQUIRE(p1 <= p3);
+        STATIC_REQUIRE(p2 > p1);
+        STATIC_REQUIRE(p2 >= p2);
+    }
+
+    SECTION("Greater the different type")
+    {
+        STATIC_REQUIRE(p1 < 10.);
+        STATIC_REQUIRE(p1 <= 1.);
+        STATIC_REQUIRE(10. > p1);
+    }
 }
 
-TEST_CASE("Factorials are computed with constexpr", "[factorial]")
+TEST_CASE("Arithmetic constexpr", "[primitive]")
 {
-STATIC_REQUIRE(Factorial(1) == 1);
-STATIC_REQUIRE(Factorial(2) == 2);
-STATIC_REQUIRE(Factorial(3) == 6);
-STATIC_REQUIRE(Factorial(10) == 3628800);
+    spdlog::set_level(log_level);
+    SECTION("Add")
+    {
+        constexpr primitive_double p1{ 1 };
+        constexpr primitive_double p2{ 2 };
+        constexpr primitive_double p_res = p1 + p2;
+
+        STATIC_REQUIRE(p_res == 3);
+    }
+
+    SECTION("Subtract")
+    {
+        constexpr primitive_double p1{ 1 };
+        constexpr primitive_double p2{ 2 };
+        constexpr primitive_double p_res = p1 - p2;
+
+        STATIC_REQUIRE(p_res == -1);
+    }
 }
